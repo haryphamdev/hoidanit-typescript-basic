aa837e7729be0a84255dd79c81aef42c0401911b
Hoi Dan IT with Eric <haryphamdev@gmail.com>
2022-09-24 13:00:51 +0700

    video 9

---
+++ projects/lesson9/lesson9.js
@@ -0,0 +1,2 @@
+let name = "Eric";
+let name1 = 'Eric';
---
+++ projects/lesson9/lesson9.ts
@@ -0,0 +1,5 @@
+let name2: string = ` và "Hỏi Dân IT"`;
+let name3 = `Eric ${name2}`; //template strings js
+
+console.log(">>> check name = ", name3)
+// Eric và "Hỏi Dân IT"

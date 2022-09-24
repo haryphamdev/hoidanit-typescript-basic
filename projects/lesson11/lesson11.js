5bbe9b44c25c39bcb2b880583274e369141b1c43
Hoi Dan IT with Eric <haryphamdev@gmail.com>
2022-09-24 13:43:03 +0700

    lesson 11

---
+++ projects/lesson11/lesson11.js
@@ -0,0 +1,6 @@
+let mine = {};
+mine = { name: "Eric" };
+
+mine.address = "Hỏi Dân IT";
+
+console.log(">>> check mine: ", mine)
---
+++ projects/lesson11/lesson11.ts
@@ -0,0 +1,20 @@
+// let person: object;
+
+// let mine3 = {name : 'hoi dan it'}
+
+// person = { name: 'Eric'};
+// person = 'Eric'
+
+// let pro = {
+//     name: "Eric", //string
+//     age: 25 //age
+// }
+
+let pro1: {
+    name: string,
+    age: number
+} = {
+    name: "Eric", //string
+    age: 25 //age
+}
+

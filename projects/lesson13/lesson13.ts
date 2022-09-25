a3974d3d65d7fca2c0cccb85321532bf44271c71
Hoi Dan IT with Eric <haryphamdev@gmail.com>
2022-09-25 13:17:22 +0700

    lesson 13

---
+++ projects/lesson13/lesson13.ts
@@ -0,0 +1,12 @@
+let skills: (string | number)[] = ['Hỏi Dân IT', 25];
+skills.push('coding');
+skills.push(69);
+
+//tuple: dataType/size/order
+let skills2: [string, number] = ['Hỏi Dân IT', 25];
+
+let skills3: [boolean, string, number?];
+
+skills3 = [true, 'Eric'];
+
+

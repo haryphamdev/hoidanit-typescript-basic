01f5dbeb372f995fd30e87a55e845409ccd3fded
Hoi Dan IT with Eric <haryphamdev@gmail.com>
2022-09-28 22:02:48 +0700

    video 14

---
+++ projects/lesson14/lesson14.ts
@@ -0,0 +1,14 @@
+enum API_STATUS {
+    PENDDING,
+    FULFILLED = "FULFILLED asfdasfasf",
+    REJECTED = "FULFILLED" //PROMISE
+}
+
+//constant
+//java
+
+let a1 = API_STATUS.PENDDING;
+let a2 = API_STATUS.FULFILLED;
+
+//frontend : syntax
+console.log(">>> a1 = ", a1, " a2 = ", a2)

49bdcb5ccea6e1846f6d7bd075062a55fb854ff4
Hoi Dan IT with Eric <haryphamdev@gmail.com>
2022-09-29 20:22:42 +0700

    video 23

--- chapter3/backup_chapter3.ts
+++ chapter3/backup_chapter3.ts
@@ -1,3 +1,30 @@
+//lesson 22
+
+for (let i = 0; i < 10; i++) {
+    console.log(">> i= ", i);
+    //i = 9 => 10
+}
+
+let i1 = 0;
+for (; i1 < 10; i1++) {
+    console.log(i1);
+}
+
+
+for (let i = 0; ; i++) {
+    console.log(i);
+    if (i > 9) break; // cần có if/break để không khiến vòng lặp chạy vô hạn
+}
+
+
+let i = 0;
+for (; ;) {
+    console.log(i);
+    i++;
+    if (i > 9) break;
+}
+
+
 //lesson21
 const age: number = 20;
 

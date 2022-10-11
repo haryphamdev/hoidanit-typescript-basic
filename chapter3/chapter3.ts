972c43a1ffdabbee3ba04484f63159549de9cf9d
Hoi Dan IT with Eric <haryphamdev@gmail.com>
2022-10-11 22:29:06 +0700

    video 33

--- chapter3/chapter3.ts
+++ chapter3/chapter3.ts
@@ -1,1 +1,19 @@
-//lesson32
+//lesson33
+class Person {
+    ssn: string;
+    firstName: string;
+    lastName: string;
+
+    constructor(ssn: string, firstName: string, lastName: string) {
+        this.ssn = ssn;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    getFullName(): string {
+        return `calling method: ${this.firstName} ${this.lastName}`;
+    }
+}
+
+let hoidanit = new Person("123", "Hoi Dan IT", " Eric");
+console.log(">> check class: ", hoidanit.getFullName())

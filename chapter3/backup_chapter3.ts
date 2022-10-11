cb28fab348e31876b36f018f24f6113216095329
Hoi Dan IT with Eric <haryphamdev@gmail.com>
2022-10-11 21:31:11 +0700

    video 31

--- chapter3/backup_chapter3.ts
+++ chapter3/backup_chapter3.ts
@@ -1,3 +1,16 @@
+//lesson 30
+
+let sum7 = (x: number, y: number, z: boolean = false) => {
+    if (z === false) {
+        return x + y;
+    }
+
+    if (z)
+        return x - y;
+}
+
+console.log(">>> check sum7 = ", sum7(1, 2), sum7(1, 2, true))
+
 //lesson 29: optional params
 
 const sum5 = (x: number, y: number, z?: number) => {
--- chapter3/chapter3.ts
+++ chapter3/chapter3.ts
@@ -1,12 +1,37 @@
-//lesson 30
-
-let sum7 = (x: number, y: number, z: boolean = false) => {
-    if (z === false) {
-        return x + y;
-    }
-
-    if (z)
-        return x - y;
+//lesson 31
+//spread syntax vs rest => copy all
+function getTotal(...numbers: number[]): number {
+    let total = 0;
+    numbers.forEach((num) => total += num);
+    //[10, 20].forEach((num) => total += num);
+    //[].forEach((num) => total += num);
+    return total;
 }
 
-console.log(">>> check sum7 = ", sum7(1, 2), sum7(1, 2, true))
+// console.log(getTotal()); // 0
+// console.log(getTotal(10, 20)); // 30
+// console.log(getTotal(10, 20, 30)); // 60
+
+function multiply(n: number, ...m: number[]) {
+    let a = [69, 96, 3, 4].map((x) => {
+        console.log("check x = ", x)
+        return n * x
+    });
+
+    return m.map((x) => n * x);
+}
+// 'a' gets value [10, 20, 30, 40]
+// const test31 = multiply(15, 69, 96, 3, 4);
+
+// console.log(">>> check test31 = ", test31)
+
+
+function Greet(greeting: string, ...names: string[]) {
+    return greeting + " " + names.join(", ") + "!";
+}
+
+console.log(Greet("Xin chao", "Eric &", "Hoi dan it")); // returns "Hello Steve, Bill!"
+
+//Greet("Hello");// returns "Hello !"
+
+

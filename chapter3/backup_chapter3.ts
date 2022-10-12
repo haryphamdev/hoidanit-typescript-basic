096717d9507763c99b207e3359f2484fb782f835
Hoi Dan IT with Eric <haryphamdev@gmail.com>
2022-10-12 21:49:43 +0700

    video 35

--- chapter3/backup_chapter3.ts
+++ chapter3/backup_chapter3.ts
@@ -1,3 +1,15 @@
+//lesson 35
+class Person1 {
+    readonly birthDate: Date;
+
+    constructor(birthDate: Date) {
+        this.birthDate = birthDate;
+    }
+}
+
+let person = new Person1(new Date(1990, 12, 25));
+// person.birthDate = new Date(1991, 12, 25); // Compile error
+
 //lesson 34
 class Employee {
     public empName: string;

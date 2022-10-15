5c1d48647255c510025f7498a59e5ae3c1734020
Hoi Dan IT with Eric <haryphamdev@gmail.com>
2022-10-15 22:08:33 +0700

    video 38

--- chapter3/backup_chapter3.ts
+++ chapter3/backup_chapter3.ts
@@ -1,3 +1,48 @@
+//lesson 37
+
+class Person3 {
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    getFullName(): string {
+        return `${this.firstName} ${this.lastName}`;
+    }
+    describe(): string {
+        return `This is ${this.firstName} ${this.lastName}.`;
+    }
+}
+
+//để kế thừa 1 class, chúng ta sử dụng keyword extends
+
+class Employee1 extends Person3 {
+    private jobTitle;
+    constructor(
+        firstName: string,
+        lastName: string,
+        jobTitle: string) {
+
+        // call the constructor of the Person class:
+        super(firstName, lastName);
+        this.jobTitle = jobTitle;
+    }
+    //overwrite
+    describe(): string {
+        return `${super.describe()} from parent - describe Hoi Dan IT`;
+    }
+}
+
+// let employee = new Employee('John','Doe','Front-end Developer');
+
+//Employee kết thừa lại person => dùng đc method của parent
+let employee = new Employee1('Hoi Dan IT', 'Eric', 'Web Developer');
+
+console.log(employee.getFullName());
+console.log(employee.describe());
+
 //lesson 36
 class Person2 {
     private _age: number;
--- chapter3/chapter3.ts
+++ chapter3/chapter3.ts
@@ -1,44 +1,12 @@
-//lesson 37
+//lesson 38
+class Circle {
+    static pi: number = 3.14;
+    public test: number = 69;
 
-class Person3 {
-    firstName: string;
-    lastName: string;
-
-    constructor(firstName: string, lastName: string) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-    getFullName(): string {
-        return `${this.firstName} ${this.lastName}`;
-    }
-    describe(): string {
-        return `This is ${this.firstName} ${this.lastName}.`;
+    static calculateArea(radius: number) {
+        return this.pi * radius * radius;
     }
 }
 
-//để kế thừa 1 class, chúng ta sử dụng keyword extends
-
-class Employee1 extends Person3 {
-    private jobTitle;
-    constructor(
-        firstName: string,
-        lastName: string,
-        jobTitle: string) {
-
-        // call the constructor of the Person class:
-        super(firstName, lastName);
-        this.jobTitle = jobTitle;
-    }
-    //overwrite
-    describe(): string {
-        return `${super.describe()} from parent - describe Hoi Dan IT`;
-    }
-}
-
-// let employee = new Employee('John','Doe','Front-end Developer');
-
-//Employee kết thừa lại person => dùng đc method của parent
-let employee = new Employee1('Hoi Dan IT', 'Eric', 'Web Developer');
-
-console.log(employee.getFullName());
-console.log(employee.describe());
+let t = new Circle();
+console.log(">>> check pi= ", Circle.calculateArea(10));

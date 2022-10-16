9efba91c50221a61118e7f19b216507a06156f8e
Hoi Dan IT with Eric <haryphamdev@gmail.com>
2022-10-16 22:11:42 +0700

    video 41

--- chapter3/backup_chapter3.ts
+++ chapter3/backup_chapter3.ts
@@ -1,3 +1,28 @@
+//lesson 40
+interface IPerson {
+    readonly firstName: string;
+    lastName?: string;
+    address: string
+}
+
+type Person41 = {
+    firstName: string;
+    lastName: string
+}
+
+function getFullName(person: IPerson) {
+    return `${person.firstName} ${person.lastName}`;
+}
+
+let person40 = {
+    firstName: 'Eric',
+    lastName: 'Hoi Dan IT',
+    address: 'Ha noi'
+};
+
+console.log(getFullName(person40)); // Eric Hoi Dan IT
+
+
 //lesson 38
 class Circle {
     static pi: number = 3.14;
--- chapter3/chapter3.ts
+++ chapter3/chapter3.ts
@@ -1,23 +1,20 @@
-//lesson 40
-interface IPerson {
-    readonly firstName: string;
-    lastName?: string;
-    address: string
+//lesson 41
+
+interface IEmployee {
+    empCode: number;
+    name: string;
+    getSalary: (empCode1: number) => number;
 }
 
-type Person41 = {
-    firstName: string;
-    lastName: string
+class Eric implements IEmployee {
+    public empCode: number;
+    public name: string;
+
+    getSalary = (code: number): number => {
+        return 100 * code;
+    }
+
+    getName = () => {
+        return 'hoi dan it'
+    }
 }
-
-function getFullName(person: IPerson) {
-    return `${person.firstName} ${person.lastName}`;
-}
-
-let person40 = {
-    firstName: 'Eric',
-    lastName: 'Hoi Dan IT',
-    address: 'Ha noi'
-};
-
-console.log(getFullName(person40)); // Eric Hoi Dan IT

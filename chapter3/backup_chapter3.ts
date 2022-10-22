39b4da6a058b411060df0a38dad39c9474f8d6e6
Hoi Dan IT with Eric <haryphamdev@gmail.com>
2022-10-22 21:44:15 +0700

    video 39

--- chapter3/backup_chapter3.ts
+++ chapter3/backup_chapter3.ts
@@ -1,3 +1,24 @@
+//lesson 41
+
+interface IEmployee {
+    empCode: number;
+    name: string;
+    getSalary: (empCode1: number) => number;
+}
+
+class Eric implements IEmployee {
+    public empCode: number;
+    public name: string;
+
+    getSalary = (code: number): number => {
+        return 100 * code;
+    }
+
+    getName = () => {
+        return 'hoi dan it'
+    }
+}
+
 //lesson 40
 interface IPerson {
     readonly firstName: string;
--- chapter3/chapter3.ts
+++ chapter3/chapter3.ts
@@ -1,20 +1,41 @@
-//lesson 41
+//lesson 39
+abstract class Employee39 {
+    constructor(private firstName: string, private lastName: string) {
+    }
+    abstract getSalary(): number; //abstract method
 
-interface IEmployee {
-    empCode: number;
-    name: string;
-    getSalary: (empCode1: number) => number;
-}
-
-class Eric implements IEmployee {
-    public empCode: number;
-    public name: string;
-
-    getSalary = (code: number): number => {
-        return 100 * code;
+    //normal method
+    get fullName(): string {
+        return `${this.firstName} ${this.lastName}`;
     }
 
-    getName = () => {
-        return 'hoi dan it'
+    compensationStatement(): string {
+        return `${this.fullName} makes ${this.getSalary()} a month.`;
     }
 }
+
+// const e1 = new Employee39("eric", "hoi dan it");
+class FullTimeEmployee extends Employee39 {
+    constructor(firstName: string, lastName: string, private salary: number) {
+        super(firstName, lastName); //dùng super để kế thừa lại cha
+    }
+
+    // cần viết method này, vì nó được khai báo abstract ở trên
+    getSalary(): number {
+        //asdfasdfasdf
+        //asdfasdfasfd
+        return this.salary;
+    }
+}
+
+class Contractor extends Employee39 {
+    constructor(firstName: string, lastName: string, private rate: number, private hours: number) {
+        super(firstName, lastName);
+    }
+    getSalary(): number {
+        return this.rate * this.hours;
+    }
+}
+
+const test1 = new FullTimeEmployee("eric", 'hoi dan it', 1000);
+console.log(">>> test 1 : ", test1.getSalary())
